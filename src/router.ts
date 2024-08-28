eb4f8af51222ec7dbf3025175f8cfbd36707bb06
khaztaroth <astaroth503@gmail.com>
2024-08-27 21:30:12 -0400

    changed draz's lore corner link

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ export async function routeRequest(request: Request) {
     const normalizedPath = path[0].toUpperCase()
 
     switch (normalizedPath) {
-        case "DRAZLORECORNER":
+        case "DRAZSOLSLORE":
             return Response.redirect("https://docs.google.com/document/d/1uUr5ODdvwRQuPjZApg6kgSoqZaUjPT0t83A8HLQwOJ0/edit#heading=h.58rejhp4l3e4")
         case "STARTRUCKER":
             return Response.redirect("https://store.steampowered.com/app/2380050/Star_Trucker/?utm_source=SSS&utm_content=Launch")
